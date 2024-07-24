9d63ec35141ff863b045a213f21a6d18591c4d45
hyhyf <627680933@qq.com>
2024-07-24 14:24:41 +0800

    Support tracking the message listener of spring jms

--- test/plugin/scenarios/spring-jms-5.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/spring/jms/controller/CaseController.java
+++ test/plugin/scenarios/spring-jms-5.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/spring/jms/controller/CaseController.java
@@ -18,8 +18,8 @@
 
 package org.apache.skywalking.apm.testcase.spring.jms.controller;
 
-// import org.apache.activemq.ActiveMQConnection;
-// import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
@@ -44,38 +44,38 @@ public class CaseController {
     @Value("${activemq.server}")
     private String brokenUrl;
 
-    // private static final String USER_NAME = ActiveMQConnection.DEFAULT_USER;
-    // private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+     private static final String USER_NAME = ActiveMQConnection.DEFAULT_USER;
+     private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
 
     private static final String SUCCESS = "Success";
 
     @RequestMapping("/spring-jms-scenario")
     @ResponseBody
     public String testcase() {
-        // Session session = null;
-        // Connection connection = null;
-        // try {
-        //     ConnectionFactory factory = new ActiveMQConnectionFactory(USER_NAME, PASSWORD, brokenUrl);
-        //     connection = factory.createConnection();
-        //     connection.start();
-        //     session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-        //     Destination destination = session.createQueue("test");
-        //     MessageProducer messageProducer = session.createProducer(destination);
-        //     TextMessage message = session.createTextMessage("test");
-        //     messageProducer.send(message);
-        //     session.commit();
-        //     session.close();
-        //     connection.close();
-        // } catch (Exception ex) {
-        //     LOGGER.error(ex);
-        //     try {
-        //         session.close();
-        //         connection.close();
-        //     } catch (JMSException e) {
-        //         LOGGER.error(e);
-        //     }
-        // }
-        // new ConsumerThread().start();
+         Session session = null;
+         Connection connection = null;
+         try {
+             ConnectionFactory factory = new ActiveMQConnectionFactory(USER_NAME, PASSWORD, brokenUrl);
+             connection = factory.createConnection();
+             connection.start();
+             session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+             Destination destination = session.createQueue("test");
+             MessageProducer messageProducer = session.createProducer(destination);
+             TextMessage message = session.createTextMessage("test");
+             messageProducer.send(message);
+             session.commit();
+             session.close();
+             connection.close();
+         } catch (Exception ex) {
+             LOGGER.error(ex);
+             try {
+                 session.close();
+                 connection.close();
+             } catch (JMSException e) {
+                 LOGGER.error(e);
+             }
+         }
+         new ConsumerThread().start();
         return SUCCESS;
     }
 
@@ -85,31 +85,31 @@ public class CaseController {
         return SUCCESS;
     }
 
-    // public class ConsumerThread extends Thread {
-    //     @Override
-    //     public void run() {
-    //         Session session = null;
-    //         Connection connection = null;
-    //         try {
-    //             ConnectionFactory factory = new ActiveMQConnectionFactory(USER_NAME, PASSWORD, brokenUrl);
-    //             connection = factory.createConnection();
-    //             connection.start();
-    //             session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-    //             Destination destination = session.createQueue("test");
-    //             MessageConsumer messageConsumer = session.createConsumer(destination);
-    //             messageConsumer.receive();
-    //             session.close();
-    //             connection.close();
-    //         } catch (Exception ex) {
-    //             LOGGER.error(ex);
-    //             try {
-    //                 session.close();
-    //                 connection.close();
-    //             } catch (JMSException e) {
-    //                 LOGGER.error(e);
-    //             }
-    //         }
-    //     }
-    // }
+     public class ConsumerThread extends Thread {
+         @Override
+         public void run() {
+             Session session = null;
+             Connection connection = null;
+             try {
+                 ConnectionFactory factory = new ActiveMQConnectionFactory(USER_NAME, PASSWORD, brokenUrl);
+                 connection = factory.createConnection();
+                 connection.start();
+                 session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+                 Destination destination = session.createQueue("test");
+                 MessageConsumer messageConsumer = session.createConsumer(destination);
+                 messageConsumer.receive();
+                 session.close();
+                 connection.close();
+             } catch (Exception ex) {
+                 LOGGER.error(ex);
+                 try {
+                     session.close();
+                     connection.close();
+                 } catch (JMSException e) {
+                     LOGGER.error(e);
+                 }
+             }
+         }
+     }
 
 }
